879033012894c39f88162732e5fa1d2158eeb1b1
annasokolnikovag <118181860+annasokolnikovag@users.noreply.github.com>
2023-02-06 09:49:48 +0300

    Add files via upload

---
+++ lesson22/lesson22/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lesson22
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string Header { get; set; }
+        public string Description { get; set; }
+        public ImageSource ColoredSource { get; set; }
+        public ImageSource BlackWhiteSource =>
+            (ColoredSource == null) ? null : new FormatConvertedBitmap((BitmapSource)ColoredSource, PixelFormats.Gray8, null, 0);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
